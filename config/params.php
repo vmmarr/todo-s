9c73db9ba350ff7d490d2eb309dc9d47baea0d9a
maria del mar <mariadelmar.vazquez@iesdonana.org>
2020-11-25 00:10:10 +0100

    Añado bootstrap y datecontrol

--- config/params.php
+++ config/params.php
@@ -1,7 +1,22 @@
 <?php
 
+use kartik\datecontrol\Module;
+
 return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'bsVersion' => '4.x',
+    'dateControlDisplay' => [
+        Module::FORMAT_DATE => 'php:d-m-Y',
+        Module::FORMAT_TIME => 'php:H:i:s',
+        Module::FORMAT_DATETIME => 'php:d-m-Y H:i:s',
+    ],
+
+    // format settings for saving each date attribute (PHP format example)
+    'dateControlSave' => [
+        Module::FORMAT_DATE => 'php:Y-m-d', // saves as unix timestamp
+        Module::FORMAT_TIME => 'php:H:i:s',
+        Module::FORMAT_DATETIME => 'php:Y-m-d H:i:s',
+    ],
 ];
